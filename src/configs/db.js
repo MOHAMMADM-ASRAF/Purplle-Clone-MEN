9f52d108551f3d419f9fe8b913109c010ac3cae2
Karu555 <karunesh029@gmail.com>
2021-12-19 19:48:51 +0530

    Create db.js

---
+++ src/configs/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');const mongoose = require('mongoose');
+
+
+
+module.exports = () => {
+    return mongoose.connect("mongodb+srv://shweta:shweta@cluster0.u1tll.mongodb.net/allProducts")
+}
