c889733ca248cdeea7157dc917e9ae5a0dd5f9d3
ShwetaMane13 <91532009+ShwetaMane13@users.noreply.github.com>
2021-12-19 11:58:34 +0530

    Delete db.js

--- src/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const connect = () => {
-    return mongoose.connect(`mongodb+srv://shweta:shweta@cluster0.u1tll.mongodb.net/allProducts`);
-}
-
-module.exports = connect;
-// XgS4xYNdggmMLPgP - atlas password
